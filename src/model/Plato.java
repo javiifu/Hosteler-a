37701bda5f0105b11d8299ca43356d0408e2de0e
PabloGargalloSanz <pablogargallosanz@gmail.com>
2025-04-16 12:58:12 +0200

    Creacion model y view de plato

---
+++ src/model/Plato.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Plato {
+    //Atributos
+    private String codigo;
+    private int espacios;
+    private Boolean estado;
+    
+    //Constructor
+    public Plato(String codigo, int espacios){
+        this.codigo = codigo;
+        this.espacios = espacios; 
+    }
+}

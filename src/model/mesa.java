7280997d7ead61b41a7a4048c0bc29367f23b5a7
Carlosdve20 <carlosdve2.0@gmail.com>
2025-04-20 15:11:42 +0200

    añadida la clase mesa

---
+++ src/model/mesa.java
@@ -0,0 +1,56 @@
+package model;
+
+public class mesa {
+    //Atributos
+    private int id;
+    private int numero;
+    private int capacidad;
+    private boolean estado; 
+
+    public void Mesa(int numero, int capacidad, boolean estado) {
+        this.numero = numero;
+        this.capacidad = capacidad;
+        this.estado = estado;
+}
+    //Constructor para consultas y actualizaciones (con el id)
+    public void Mesa(int id, int numero, int capacidad, boolean estado) {
+    this.id = id;
+    this.numero = numero;
+    this.capacidad = capacidad;
+    this.estado = estado;
+}
+    //Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
+
+

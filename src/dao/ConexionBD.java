6491d4a32e9eb751dd25c075ae273265c889ed1c
Pablo Gargallo <pablogargallosanz@gmail.com>
2025-04-29 10:43:54 +0200

    Feat: Modificacion connexion con la BD correcta, modificado ConexionBD y creado carpeta lib

--- src/dao/ConexionBD.java
+++ src/dao/ConexionBD.java
@@ -6,9 +6,10 @@ import java.sql.SQLException;
 
 //Se tendra que poner el usuario y contraseña que nos diga Saul
 public class ConexionBD {
-    private static final String URL = "jdbc:mysql://localhost:3306/Hosteleria";
-    private static final String USUARIO = "dev";
-    private static final String CONTRASENA = "password";
+    private static final String URL = "jdbc:mysql://preda.es:4141/restaurante";
+    private static final String USUARIO = "restaurante";
+    private static final String CONTRASENA = "GestorRestaurante5241";
+
 
 
 //Metodo para establecer conexion con la DB con el uso de la URL, usuario y contraseña

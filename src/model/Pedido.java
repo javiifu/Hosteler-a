583db523b6b4aff007826c0d54b35ec7a0feb437
Pablo Gargallo <pablogargallosanz@gmail.com>
2025-04-29 14:19:06 +0200

    Feat: Insertar platos a pedido, crear model pedido y model pedidoPlato

---
+++ src/model/Pedido.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Pedido {
+    private int id;
+    private int numeroMesa;
+    private LocalDateTime horaPedido;
+
+    // Constructor 
+    public Pedido(int numeroMesa, LocalDateTime horaPedido) {
+        this.numeroMesa = numeroMesa;
+        this.horaPedido = horaPedido;
+    }
+
+    // Getters y setters para todos los atributos
+    public int getId() { return id;}
+    public int getNumeroMesa() {return numeroMesa; }
+    public LocalDateTime getHoraPedido() { return horaPedido; }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+               "id=" + id +
+               ", numeroMesa=" + numeroMesa +
+               ", horaPedido=" + horaPedido +
+               '}';
+    }
+}

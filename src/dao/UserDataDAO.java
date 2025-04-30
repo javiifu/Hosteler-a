92d152c7a078c413458dc8e6bdb2b4fcb1a31c99
Carlos Delgado <carlos.dz16180@gmail.com>
2025-04-30 09:25:14 +0200

    Fix: Coherencia de UserDataDAO ajustada segun la DB

--- src/dao/UserDataDAO.java
+++ src/dao/UserDataDAO.java
@@ -8,14 +8,14 @@ import model.UserData;
 
 public class UserDataDAO {
     public static boolean validarCredenciales(String user, String password){
-        String sql = "SELECT password FROM users WHERE username = ?";
+        String sql = "SELECT contraseña FROM Usuarios WHERE nombre_usuario = ?";
         Connection conexion = ConexionBD.conectar();
         try {
             PreparedStatement statement = conexion.prepareStatement(sql);
             statement.setString(1, user);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                String passwordBD = resultSet.getString("password");
+                String passwordBD = resultSet.getString("contraseña");
                 return password.equals(passwordBD);
             }
         } catch (SQLException e) {
@@ -33,14 +33,14 @@ public class UserDataDAO {
     }
 
     public static UserData obtenerUsuario(String user) {
-        String sql = "SELECT * FROM users WHERE username = ?";
+        String sql = "SELECT * FROM Usuarios WHERE nombre_usuario = ?";
         Connection conexion = ConexionBD.conectar();
         try {
             PreparedStatement statement = conexion.prepareStatement(sql);
             statement.setString(1, user);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                return new UserData(resultSet.getInt("id"), resultSet.getBoolean("isAdmin"), resultSet.getString("user"), resultSet.getString("password"));
+                return new UserData(resultSet.getInt("id"), resultSet.getBoolean("administrador"), resultSet.getString("nombre_usuario"), resultSet.getString("contraseña"));
             }
         } catch (SQLException e) {
             System.out.println("Error al obtener usuario: " + e.getMessage());

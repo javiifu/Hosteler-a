fe1c1d14c70a92236a11bef8723533b467d78ae0
Pablo Gargallo <pablogargallosanz@gmail.com>
2025-05-07 12:19:47 +0200

    Fix Tostring producto

--- src/model/Producto.java
+++ src/model/Producto.java
@@ -62,14 +62,7 @@ public class Producto {
 
     @Override
     public String toString() {
-        return "Producto {" +
-               "\n  Código: " + codigo +
-               ",\n  Nombre: " + nombre +
-               ",\n  Descripción: " + descripcion +
-               ",\n  Precio: " + precio +
-               ",\n  Disponible: " + (disponible ? "Sí" : "No") +
-               ",\n  Código de categoría: " + categoriaCodigo +
-               "\n}";
+        return  nombre ;
     }
 
 }

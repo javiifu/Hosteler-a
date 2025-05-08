dc2c99a34ee536719e2756eb597f758e6332edf6
Pablo Gargallo <pablogargallosanz@gmail.com>
2025-05-08 13:25:23 +0200

    Feat: Modificar modificar producto

--- src/view/VistaConfiguracion.java
+++ src/view/VistaConfiguracion.java
@@ -24,24 +24,28 @@ public class VistaConfiguracion extends JPanel{
         add(titulo, BorderLayout.NORTH);
 
         // Panel central con botones
-        JPanel panelOpciones = new JPanel(new GridLayout(4, 2, 20, 20));
+        JPanel panelOpciones = new JPanel(new GridLayout(5, 1, 20, 20));
         panelOpciones.setBackground(ColorPaleta.FONDO_SECUNDARIO);
 
         // Botones para las opciones
         Boton botonCrearPlato = new Boton("Crear Plato", ColorPaleta.ENFASIS_ACCION, ColorPaleta.HOVER_ENFASIS_ACCION);
         botonCrearPlato.addActionListener(e -> abrirFormularioProducto("Crear Producto"));
+
+        Boton botonModificarPlato = new Boton("Modificar Plato",  ColorPaleta.ACENTO, ColorPaleta.HOVER_ACENTO);
+        botonModificarPlato.addActionListener(e -> abrirFormularioProducto("Modificar Producto"));
         
-        Boton botonEliminarPlato = new Boton("Eliminar Plato", ColorPaleta.ACENTO, ColorPaleta.HOVER_ACENTO);
+        Boton botonEliminarPlato = new Boton("Eliminar Plato",ColorPaleta.ENFASIS_ACCION, ColorPaleta.HOVER_ENFASIS_ACCION);
         botonEliminarPlato.addActionListener(e -> abrirFormularioProducto("Eliminar Producto"));
         
-        Boton botonCrearCategoria = new Boton("Crear Categoría", ColorPaleta.ENFASIS_ACCION, ColorPaleta.HOVER_ENFASIS_ACCION);
+        Boton botonCrearCategoria = new Boton("Crear Categoría", ColorPaleta.ACENTO, ColorPaleta.HOVER_ACENTO);
         botonCrearCategoria.addActionListener(e -> abrirFormularioCategoria("Crear Categoría"));
         
-        Boton botonEliminarCategoria = new Boton("Eliminar Categoría", ColorPaleta.ACENTO, ColorPaleta.HOVER_ACENTO);
+        Boton botonEliminarCategoria = new Boton("Eliminar Categoría", ColorPaleta.ENFASIS_ACCION, ColorPaleta.HOVER_ENFASIS_ACCION);
         botonEliminarCategoria.addActionListener(e -> abrirFormularioCategoria("Eliminar Categoría"));
 
         // Añadir botones al panel
         panelOpciones.add(botonCrearPlato);
+        panelOpciones.add(botonModificarPlato);
         panelOpciones.add(botonEliminarPlato);
         panelOpciones.add(botonCrearCategoria);
         panelOpciones.add(botonEliminarCategoria);
@@ -56,12 +60,15 @@ public class VistaConfiguracion extends JPanel{
 
     // Metodo para abrir formulario para platos
     private void abrirFormularioProducto(String tipo) {
+        ProductoDAO productoDAO = new ProductoDAO();
+
         JDialog dialog = new JDialog(tpvMain, tipo, true);
         dialog.setSize(600, 250);
-        dialog.setLayout(new GridLayout(3, 4, 10, 10));
+        dialog.setLayout(new GridLayout(4, 4, 10, 10));
         dialog.setLocationRelativeTo(this);
 
         JLabel labelNombre = new JLabel("Nombre del Producto:");
+        JLabel labelNuevoNombre = new JLabel("Nuevo nombre:");
         JTextField campoNombre = new JTextField();
         JComboBox<String> comboProductos = new JComboBox<>();
 
@@ -80,13 +87,18 @@ public class VistaConfiguracion extends JPanel{
             }
         }
 
+        
+       /* JLabel labelDescripcion = new JLabel("Descripción:");
+        JTextArea campoDescripcion = new JTextArea(); */
+
         JLabel labelDescripcion = new JLabel("Descripción:");
-        JTextArea campoDescripcion = new JTextArea();
+        JTextField campoDescripcion = new JTextField();
 
         JLabel labelPrecio = new JLabel("<html> Precio: <br> Ejemplo 0.0 <html>");
         JTextField campoPrecio = new JTextField();
 
         JLabel labelCategoria = new JLabel("Categoría:");
+        JLabel labelCategoriaModificar = new JLabel("<html> Categoría: <br> <font color='red'> Fijarse en la categoria <html>");
         comboCategorias.removeAllItems(); // Limpia el combo si ya tiene datos
         cargarCategoriasEnCombo(comboCategorias); // Método para cargar categorías en el combo
         
@@ -94,7 +106,8 @@ public class VistaConfiguracion extends JPanel{
         botonGuardar.addActionListener(e -> {
             String nombre = null;
             String categoria = (String) comboCategorias.getSelectedItem();
-        
+            boolean modificado = false;
+
             if (tipo.equals("Eliminar Producto")) {
                 nombre = (String) comboProductos.getSelectedItem(); // desplegable
                 if (nombre == null || categoria == null) {
@@ -102,7 +115,7 @@ public class VistaConfiguracion extends JPanel{
                     return;
                 }
                 try {
-                    ProductoDAO productoDAO = new ProductoDAO();
+                    
                     productoDAO.borrarProductoPorNombre(nombre);
                     JOptionPane.showMessageDialog(dialog, "Producto eliminado con éxito.");
                     dialog.dispose(); // Cerrar el diálogo después de guardar
@@ -128,12 +141,59 @@ public class VistaConfiguracion extends JPanel{
                     CategoriaDAO categoriaDAO = new CategoriaDAO();
                     int idCategoria = categoriaDAO.ObtenerCategoriaPorNombre(categoria);
         
-                    ProductoDAO productoDAO = new ProductoDAO();
                     productoDAO.crearProducto(nombre, descripcion, precio, idCategoria);
                     JOptionPane.showMessageDialog(dialog, "Producto creado con éxito.");
                     dialog.dispose(); // Cerrar el diálogo después de guardar
                 } catch (NumberFormatException ex) {
                     JOptionPane.showMessageDialog(dialog, "El precio debe ser un número válido.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+
+            } else if (tipo.equals("Modificar Producto")) {
+                String nombreAntiguo = (String) comboProductos.getSelectedItem();
+                nombre = campoNombre.getText();
+                String descripcion = campoDescripcion.getText();
+                String precioTexto = campoPrecio.getText();
+                categoria = (String) comboCategorias.getSelectedItem();
+                int idProductoAModificar = -1;
+                modificado = false;
+            
+                try {
+                    idProductoAModificar = productoDAO.obtenerIdProductoPorNombre(nombreAntiguo);
+            
+                    if (idProductoAModificar != -1) {
+                        if (!nombre.isEmpty()) {
+                            modificado = productoDAO.actualizarCampoProducto("nombre", idProductoAModificar, nombre) || modificado;
+                        }
+                        if (!descripcion.isEmpty()) {
+                            modificado = productoDAO.actualizarCampoProducto("descripcion", idProductoAModificar, descripcion) || modificado;
+                        }
+                        if (!precioTexto.isEmpty()) {
+                            try {
+                                double precio = Double.parseDouble(precioTexto);
+                                modificado = productoDAO.actualizarCampoProducto("precio", idProductoAModificar, precio) || modificado;
+                            } catch (NumberFormatException ex) {
+                                JOptionPane.showMessageDialog(dialog, "El precio debe ser un número válido.", "Error", JOptionPane.ERROR_MESSAGE);
+                                return;
+                            }
+                        }
+                        if (categoria != null) {
+                            CategoriaDAO categoriaDAO = new CategoriaDAO();
+                            Integer idCategoria = categoriaDAO.ObtenerCategoriaPorNombre(categoria);
+                            modificado = productoDAO.actualizarCampoProducto("id_categoria", idProductoAModificar, idCategoria) || modificado;
+                        }
+            
+                        if (modificado) {
+                            JOptionPane.showMessageDialog(dialog, "Producto modificado con éxito.");
+                            dialog.dispose();
+                        } else {
+                            JOptionPane.showMessageDialog(dialog, "No se modificó ningún campo.",  "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(dialog, "Error: No se encontró el producto a modificar.", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                } catch (Exception ex) {
+                    System.err.println("Error al obtener o modificar el producto: " + ex.getMessage());
+                    JOptionPane.showMessageDialog(dialog, "Error al intentar modificar el producto.", "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
@@ -141,15 +201,28 @@ public class VistaConfiguracion extends JPanel{
         dialog.add(labelNombre);
         if (tipo.equals("Eliminar Producto")) {
             dialog.add(comboProductos);//desplegable
-
-        } else {
+        } else if (tipo.equals("Modificar Producto")) {
+            cargarTodosProductosCombo(comboProductos);
+            dialog.add(labelNombre);
+            dialog.add(comboProductos);//desplegable
+            dialog.add(new JLabel()); // Espacio vacio
+            dialog.add(new JLabel()); // Espacio vacio
+            dialog.add(labelNuevoNombre);
             dialog.add(campoNombre);
             dialog.add(labelDescripcion);
             dialog.add(campoDescripcion);
             dialog.add(labelPrecio);
             dialog.add(campoPrecio);
+            dialog.add(labelCategoriaModificar);
+            
+        }else  {
+            dialog.add(campoNombre);
+            dialog.add(labelDescripcion);
+            dialog.add(campoDescripcion);
+            dialog.add(labelPrecio);
+            dialog.add(campoPrecio);
+            dialog.add(labelCategoria);
         }
-        dialog.add(labelCategoria);
         dialog.add(comboCategorias);
         dialog.add(new JLabel()); // Espacio vacio
         dialog.add(botonGuardar);
@@ -225,6 +298,15 @@ public class VistaConfiguracion extends JPanel{
         comboProductos.removeAllItems();
         ProductoDAO productoDAO = new ProductoDAO();
         ArrayList<String> productos = productoDAO.obtenerNombresPorCategoria(nombreCategoria);
+        for (String nombre : productos) {
+            comboProductos.addItem(nombre);
+        }
+    }
+    //Metodo abrir desplegable todos productos
+    private void cargarTodosProductosCombo(JComboBox<String> comboProductos) {
+        comboProductos.removeAllItems();
+        ProductoDAO productoDAO = new ProductoDAO();
+        ArrayList<String> productos = productoDAO.nombresProdcutoArray();
         for (String nombre : productos) {
             comboProductos.addItem(nombre);
         }

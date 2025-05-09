dfd60c3909f29a68e1708ea6cd96285a74ddb01a
Javiifu <javibeltra89@gmail.com>
2025-05-09 13:49:14 +0200

    feat: Cambios en el run.bat donde se pone el comando también en linux.

--- Run.bat
+++ Run.bat
@@ -1,2 +1,7 @@
+REM Windows
+
 javac -d bin -cp .;lib\jackson-annotations-3.0-rc3.jar;lib\jackson-core-2.19.0-rc2.jar;lib\jackson-databind-2.19.0-rc2.jar;lib\mysql-connector-j-9.2.0.jar src\dao\*.java src\view\*.java src\model\*.java src\main\*.java src\config\*.java
-java -cp ".\bin;.\lib\jackson-annotations-3.0-rc3.jar;.\lib\jackson-core-2.19.0-rc2.jar;.\lib\jackson-databind-2.19.0-rc2.jar;.\lib\mysql-connector-j-9.2.0.jar" main/App
+java -cp ".\bin;.\lib\jackson-annotations-3.0-rc3.jar;.\lib\jackson-core-2.19.0-rc2.jar;.\lib\jackson-databind-2.19.0-rc2.jar;.\lib\mysql-connector-j-9.2.0.jar" \main\App.java
+REM Linux/Mac
+javac -d bin -cp .:lib/* src/dao/*.java src/view/*.java src/model/*.java src/main/*.java src/config/*.java
+java -cp ".:bin:lib/*" main.App
